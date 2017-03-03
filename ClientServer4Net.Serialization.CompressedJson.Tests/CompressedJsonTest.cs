48830f66147216624121cd034e03c8c69afa0188
dmitriy.selischev <dmitriy.selischev@logicify.com>
2017-03-03 02:56:45 +0200

    implemented

---
+++ ClientServer4Net.Serialization.CompressedJson.Tests/CompressedJsonTest.cs
@@ -0,0 +1,24 @@
+﻿using ClientServer4Net.Serialization.CompressedJson.Tests.Messages;
+using NUnit.Framework;
+
+namespace ClientServer4Net.Serialization.CompressedJson.Tests
+{
+    [TestFixture]
+    public class CompressedJsonTest
+    {
+        [Test]
+        public void TestSerializeAndDeserialize()
+        {
+            var serializer = new Serializer();
+            var message = new SimpleMessage();
+            message.Field1 = "asdasdasdas";
+            message.Field2 = 252324;
+            var data = serializer.Serialize(message);
+            var msg = serializer.Deserialize(data);
+            Assert.IsTrue(msg is SimpleMessage);
+            var simpleMessage = (SimpleMessage) msg;
+            Assert.AreEqual(message.Field1, simpleMessage.Field1);
+            Assert.AreEqual(message.Field2, simpleMessage.Field2);
+        }
+    }
+}
---
+++ ClientServer4Net.Serialization.CompressedJson.Tests/Messages/SimpleMessage.cs
@@ -0,0 +1,10 @@
+﻿using ClientServer4Net.Messages;
+
+namespace ClientServer4Net.Serialization.CompressedJson.Tests.Messages
+{
+    public class SimpleMessage : IMessage
+    {
+        public string Field1 { get; set; }
+        public int Field2 { get; set; }
+    }
+}
